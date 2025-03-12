ed0a17ba09772468da738e2bd90ec535b3876190
vidhya sagar <vidhyasagargslv@gmail.com>
2025-03-12 20:57:00 +0530

    remove Counter component and related Redux slice; update store configuration

--- lib/store.ts
+++ lib/store.ts
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './features/authSlice'
-import counterslice from './features/CounterSlice'
+
 export const makeStore = () => {
   return configureStore({
     reducer: {
       auth: authReducer,
-      counter:counterslice
+      
     },
   })
 }
@@ -15,3 +15,4 @@ export type AppStore = ReturnType<typeof makeStore>
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<AppStore['getState']>
 export type AppDispatch = AppStore['dispatch']
+
